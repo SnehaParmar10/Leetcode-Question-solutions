13acbe6d15e0efeefbcc27d2ee6bf0ffef31f89a
SnehaParmar10 <sneha.parmar3222@gmail.com>
2026-03-30 20:14:09 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (36.52%) - LeetSync

---
+++ 1041-available-captures-for-rook/available-captures-for-rook.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int numRookCaptures(char[][] board) {
+        int r = 0, c = 0;       
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                if(board[i][j] == 'R'){
+                    r = i;
+                    c = j;
+                }
+            }
+        }     
+        int count = 0;     
+        for(int i = r - 1; i >= 0; i--){
+            if(board[i][c] == 'B') break;
+            if(board[i][c] == 'p'){
+                count++;
+                break;
+            }
+        }       
+        for(int i = r + 1; i < 8; i++){
+            if(board[i][c] == 'B') break;
+            if(board[i][c] == 'p'){
+                count++;
+                break;
+            }
+        }      
+        for(int j = c - 1; j >= 0; j--){
+            if(board[r][j] == 'B') break;
+            if(board[r][j] == 'p'){
+            
+                count++;
+                break;
+            }
+        }
+        for(int j = c + 1; j < 8; j++){
+            if(board[r][j] == 'B') break;
+            if(board[r][j] == 'p'){
+                count++;
+                break;
+                
+            }
+        } 
+        return count;
+    }
+}
