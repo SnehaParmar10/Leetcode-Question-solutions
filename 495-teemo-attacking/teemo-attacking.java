a4a415ea08e0d42de34a8a8de8f4a0e004bd1254
SnehaParmar10 <sneha.parmar3222@gmail.com>
2026-04-06 22:39:26 +0530

    Time: 3 ms (90.33%) | Memory: 48.3 MB (44.83%) - LeetSync

--- 495-teemo-attacking/teemo-attacking.java
+++ 495-teemo-attacking/teemo-attacking.java
@@ -4,6 +4,7 @@ class Solution {
         
         for(int i = 0; i < timeSeries.length - 1; i++){
             total += Math.min(duration, timeSeries[i+1] - timeSeries[i]);
+            
         }
         
         return total + duration;
