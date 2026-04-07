9947a8dd5f3031f8e659cd77e1355be809ef6843
SnehaParmar10 <sneha.parmar3222@gmail.com>
2026-04-07 22:17:45 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (15.43%) - LeetSync

---
+++ 933-increasing-order-search-tree/increasing-order-search-tree.java
@@ -0,0 +1,21 @@
+class Solution {
+    TreeNode head = new TreeNode(0);
+    TreeNode curr = head;
+
+    public TreeNode increasingBST(TreeNode root) {
+        inorder(root);
+        return head.right;
+    }
+
+    private void inorder(TreeNode node) {
+        if(node == null) return;
+
+        inorder(node.left);
+
+        node.left = null;
+        curr.right = node;
+        curr = node;
+
+        inorder(node.right);
+    }
+}
