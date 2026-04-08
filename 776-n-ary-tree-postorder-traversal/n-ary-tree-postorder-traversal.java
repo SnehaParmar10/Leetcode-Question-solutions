6d75c7d26721738eccada75af1c5f85d2dc9b111
SnehaParmar10 <sneha.parmar3222@gmail.com>
2026-04-08 23:02:30 +0530

    Time: 4 ms (14.01%) | Memory: 46.5 MB (59.80%) - LeetSync

---
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        LinkedList<Integer> res = new LinkedList<>();
+        if (root == null) return res;
+        
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            res.addFirst(node.val);
+            
+            for (Node child : node.children) {
+                stack.push(child);
+            }
+        }
+        
+        return res;
+    }
+}
