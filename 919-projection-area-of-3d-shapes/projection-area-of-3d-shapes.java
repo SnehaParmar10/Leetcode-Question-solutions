6a00c94a2843dd9d650327b7f77c52eab2ed4c78
SnehaParmar10 <sneha.parmar3222@gmail.com>
2026-04-26 21:44:24 +0530

    Time: 2 ms (98.27%) | Memory: 46.3 MB (47.95%) - LeetSync

--- 919-projection-area-of-3d-shapes/projection-area-of-3d-shapes.java
+++ 919-projection-area-of-3d-shapes/projection-area-of-3d-shapes.java
@@ -4,14 +4,16 @@ class Solution {
         int top = 0, front = 0, side = 0;
 
         for (int i = 0; i < n; i++) {
+
             int rowMax = 0;
             int colMax = 0;
 
             for (int j = 0; j < n; j++) {
+                
                 if (grid[i][j] > 0) top++;
 
                 rowMax = Math.max(rowMax, grid[i][j]);
-                
+
                 colMax = Math.max(colMax, grid[j][i]);
             }
 
