07af1c2bd8b24809ca6d6e509923d42e5409a4c8
SnehaParmar10 <sneha.parmar3222@gmail.com>
2026-04-30 23:52:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (22.03%) - LeetSync

---
+++ 910-nth-magical-number/nth-magical-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int nthMagicalNumber(int n, int a, int b) {
+        long mod = 1000000007;
+        long lcm = (long)a * b / gcd(a, b);
+        
+        long low = 1, high = (long)n * Math.min(a, b);
+        
+        while (low < high) {
+            long mid = low + (high - low) / 2;
+            long count = mid / a + mid / b - mid / lcm;
+            
+            if (count < n) low = mid + 1;
+            else high = mid;
+        }
+        
+        return (int)(low % mod);
+    }
+    
+    private long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
