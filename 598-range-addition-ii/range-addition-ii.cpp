ea93aeecbbe989c2bdf0929c0b5257bbb2ada326
SnehaParmar10 <sneha.parmar3222@gmail.com>
2026-05-24 23:22:14 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (46.63%) - LeetSync

--- 598-range-addition-ii/range-addition-ii.cpp
+++ 598-range-addition-ii/range-addition-ii.cpp
@@ -1,6 +1,9 @@
 class Solution {
+
 public:
+
     int maxCount(int m, int n, vector<vector<int>>& ops) {
+        
         for (auto &op : ops) {
             m = min(m, op[0]);
             n = min(n, op[1]);
