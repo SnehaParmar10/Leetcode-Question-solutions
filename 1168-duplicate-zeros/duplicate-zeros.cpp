18a5927514dc57c46ce5965335f56b79391b2361
SnehaParmar10 <sneha.parmar3222@gmail.com>
2026-05-29 23:26:52 +0530

    Time: 3 ms (31.14%) | Memory: 14.5 MB (5.69%) - LeetSync

---
+++ 1168-duplicate-zeros/duplicate-zeros.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> temp;
+        for(int i = 0; i < n; i++) {
+            temp.push_back(arr[i]);
+
+            if(arr[i] == 0) {
+                temp.push_back(0);
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            arr[i] = temp[i];
+        }
+    }
+};
