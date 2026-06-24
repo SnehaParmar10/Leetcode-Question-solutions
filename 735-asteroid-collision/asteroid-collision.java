c8e90418c982f751756643c64c33c25371d72cd7
SnehaParmar10 <sneha.parmar3222@gmail.com>
2026-06-24 11:20:26 +0530

    Time: 5 ms (68.35%) | Memory: 47 MB (57.43%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,12 +1,8 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-
         Stack<Integer> st = new Stack<>();
-
         for (int ast : asteroids) {
-
             boolean destroyed = false;
-
             while (!st.isEmpty() &&
                    ast < 0 &&
                    st.peek() > 0) {
@@ -24,18 +20,15 @@ class Solution {
                     break;
                 }
             }
-
             if (!destroyed) {
                 st.push(ast);
             }
         }
-
         int[] ans = new int[st.size()];
 
         for (int i = st.size() - 1; i >= 0; i--) {
             ans[i] = st.pop();
         }
-
         return ans;
     }
 }
