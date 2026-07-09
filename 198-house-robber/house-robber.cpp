349e9364224b93968312cd2c2f38aba50a70e035
SnehaParmar10 <sneha.parmar3222@gmail.com>
2026-07-09 11:46:40 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (23.23%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -4,20 +4,18 @@ public:
         if (curr >= nums.size()) {
             return 0;
         }
-
         if (memo.find(curr) != memo.end()) {
             return memo[curr];
         }
-
         int rob = nums[curr] + maxProfit(curr + 2, nums, memo);
         int notRob = maxProfit(curr + 1, nums, memo);
 
         memo[curr] = max(rob, notRob);
         return memo[curr];
     }
-
     int rob(vector<int>& nums) {
         unordered_map<int, int> memo;
         return maxProfit(0, nums, memo);
+        
     }
 };
