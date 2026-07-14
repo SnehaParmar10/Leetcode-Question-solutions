0005b320aa0eb63b243b236b8c7e96fc811968e5
SnehaParmar10 <sneha.parmar3222@gmail.com>
2026-07-14 10:14:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (11.90%) - LeetSync

---
+++ 289-game-of-life/game-of-life.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void gameOfLife(int[][] board) {
+        int m = board.length;
+        int n = board[0].length;
+        int[] dr = {-1,-1,-1,0,0,1,1,1};
+        int[] dc = {-1,0,1,-1,1,-1,0,1};
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int live = 0;
+                for (int k = 0; k < 8; k++) {
+                    int nr = i + dr[k];
+                    int nc = j + dc[k];
+                    if (nr >= 0 && nr < m && nc >= 0 && nc < n) {
+                        if (board[nr][nc] == 1 || board[nr][nc] == -1)
+                            live++;
+                    }
+                }
+                if (board[i][j] == 1) {
+                    if (live < 2 || live > 3)
+                        board[i][j] = -1;
+                } else {
+                    if (live == 3)
+                        board[i][j] = 2;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == -1)
+                    board[i][j] = 0;
+                else if (board[i][j] == 2)
+                    board[i][j] = 1;
+            }
+        }
+    }
+}
